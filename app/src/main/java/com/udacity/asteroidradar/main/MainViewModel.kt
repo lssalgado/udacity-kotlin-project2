028149ea65552e7b883205d39a240991cea8d8f1
lsalgado <lsalgado@muxi.com.br>
2021-07-19 20:13:19 -0300

    Adding ImageOfTheDay livedata.

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,10 +1,13 @@
 package com.udacity.asteroidradar.main
 
-import android.net.Network
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.udacity.asteroidradar.BuildConfig
 import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import com.udacity.asteroidradar.api.parseImageOfTheDay
+import com.udacity.asteroidradar.network.ImageOfTheDay
 import com.udacity.asteroidradar.network.NasaApi
 import kotlinx.coroutines.launch
 import org.json.JSONObject
@@ -14,11 +17,18 @@ const val KEY = BuildConfig.NASA_API_KEY
 
 class MainViewModel : ViewModel() {
 
+    private val imageOfTheDay_ = MutableLiveData<ImageOfTheDay>()
+    val imageOfTheDay: LiveData<ImageOfTheDay>
+        get() = imageOfTheDay_
+
     init {
         viewModelScope.launch {
 
             try {
-                val resultList = NasaApi.retrofitService.getAsteroids("2021-07-12", "2021-07-15", KEY)
+                val imageResult = NasaApi.retrofitService.getImageOfTheDay(KEY)
+                imageOfTheDay_.value = parseImageOfTheDay(JSONObject(imageResult))
+                val resultList =
+                    NasaApi.retrofitService.getAsteroids("2021-07-12", "2021-07-15", KEY)
                 val asteroidList = parseAsteroidsJsonResult(JSONObject(resultList))
                 asteroidList.forEach {
                     Timber.e(it.toString())
