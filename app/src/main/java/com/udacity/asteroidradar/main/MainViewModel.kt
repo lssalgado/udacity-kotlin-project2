8ec0b61d7aaddd7a1a895bb2ba804e0ee16948d4
lsalgado <lsalgado@muxi.com.br>
2021-07-19 19:02:18 -0300

    Adding first test implementation.

--- app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
+++ app/src/main/java/com/udacity/asteroidradar/main/MainViewModel.kt
@@ -1,6 +1,36 @@
 package com.udacity.asteroidradar.main
 
+import android.net.Network
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.udacity.asteroidradar.api.parseAsteroidsJsonResult
+import com.udacity.asteroidradar.network.NasaApi
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+import timber.log.Timber
+
+const val KEY = ""
 
 class MainViewModel : ViewModel() {
+
+    init {
+        viewModelScope.launch {
+
+            try {
+//                Timber.e("imageOfTheDay")
+//                val imageOfTheDay = NasaApi.retrofitService.getImageOfTheDay(KEY)
+//                Timber.e(imageOfTheDay.toString())
+                Timber.e("resultList")
+                val resultList = NasaApi.retrofitService.getAsteroids("2021-07-05", KEY)
+//                val resultList = NasaApi.retrofitService.getAsteroids("2021-07-12", "2021-07-15", KEY)
+                Timber.e(resultList)
+                val asteroidList = parseAsteroidsJsonResult(JSONObject(resultList))
+                asteroidList.forEach {
+                    Timber.e(it.toString())
+                }
+            } catch (e: Exception) {
+                Timber.e(e)
+            }
+        }
+    }
 }
