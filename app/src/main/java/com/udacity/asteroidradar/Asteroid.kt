ff2f6b6eb1609bfb0022d8dd0ce3cb021bc0ae8a
lsalgado <lsalgado@muxi.com.br>
2021-07-23 19:38:09 -0300

    Adding content descriptions.

--- app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
+++ app/src/main/java/com/udacity/asteroidradar/Asteroid.kt
@@ -1,14 +1,25 @@
 package com.udacity.asteroidradar
 
+import android.content.Context
 import android.os.Parcelable
 import com.udacity.asteroidradar.database.DatabaseAsteroid
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
-data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
-                    val absoluteMagnitude: Double, val estimatedDiameter: Double,
-                    val relativeVelocity: Double, val distanceFromEarth: Double,
-                    val isPotentiallyHazardous: Boolean) : Parcelable
+data class Asteroid(
+    val id: Long, val codename: String, val closeApproachDate: String,
+    val absoluteMagnitude: Double, val estimatedDiameter: Double,
+    val relativeVelocity: Double, val distanceFromEarth: Double,
+    val isPotentiallyHazardous: Boolean
+) : Parcelable {
+    fun getContentDescription(context: Context): String {
+        return if (isPotentiallyHazardous) {
+            context.getString(R.string.potentially_hazardous)
+        } else {
+            context.getString(R.string.not_hazardous)
+        }
+    }
+}
 
 fun List<Asteroid>.asDatabaseModel(): Array<DatabaseAsteroid> {
     return map {
